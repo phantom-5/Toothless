b90b29b9db1305b6b84835469044609a3b57a651
phantom5 <rickrudra@gmail.com>
2018-05-27 08:39:55 +0530

    getting latitude and longitude of current loc

--- Toothless_Interact/app/src/main/java/com/example/rudra/toothless/WalkMode.java
+++ Toothless_Interact/app/src/main/java/com/example/rudra/toothless/WalkMode.java
@@ -1,36 +1,67 @@
 package com.example.rudra.toothless;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.speech.tts.TextToSpeech;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.widget.WrapperListAdapter;
-
 import com.robertsimoes.quoteable.QuotePackage;
 import com.robertsimoes.quoteable.Quoteable;
-
-import org.w3c.dom.Text;
-
 import java.util.Locale;
 import java.util.Random;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+//import com.google.android.gms.maps.GoogleMap;
 
-public class WalkMode extends Activity implements Quoteable.ResponseReadyListener{
+public class WalkMode extends Activity implements Quoteable.ResponseReadyListener,ConnectionCallbacks,
+        OnConnectionFailedListener,
+        LocationListener {
 
     TextToSpeech tts;
     int result_tts;
     String x_tts;
     Button walk_mode_abort;
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private double currentLatitude;
+    private double currentLongitude;
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.walk_mode);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                // The next two lines tell the new client that “this” current class will handle connection stuff
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                //fourth line adds the LocationServices API endpoint from GooglePlayServices
+                .addApi(LocationServices.API)
+                .build();
+
+        // Create the LocationRequest object
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(10 * 1000)        // 10 seconds, in milliseconds
+                .setFastestInterval(1 * 1000); // 1 second, in millisecond
+
         walk_mode_abort=findViewById(R.id.walk_mode_abort);
         walk_mode_abort.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,6 +107,109 @@ public class WalkMode extends Activity implements Quoteable.ResponseReadyListene
         };
         cd.start();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+            mGoogleApiClient.disconnect();
+        }
+    }
+    @Override
+    public void onConnected(Bundle bundle) {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(WalkMode.this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(WalkMode.this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(WalkMode.this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        111);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+            Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+            if (location == null) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+            } else {
+                //If everything went fine lets get latitude and longitude
+                currentLatitude = location.getLatitude();
+                currentLongitude = location.getLongitude();
+
+                Log.d("RICKY","lat= "+currentLatitude);
+                Log.d("RICKY","long="+currentLongitude);
+            }
+        }
+
+    }
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        /*
+         * Google Play services can resolve some errors it detects.
+         * If the error has a resolution, try sending an Intent to
+         * start a Google Play services activity that can resolve
+         * error.
+         */
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+                /*
+                 * Thrown if Google Play services canceled the original
+                 * PendingIntent
+                 */
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            /*
+             * If no resolution is available, display a dialog to the
+             * user with the error.
+             */
+            Log.e("Error", "Location services connection failed with code " + connectionResult.getErrorCode());
+        }
+    }
+
+    /**
+     * If locationChanges change lat and long
+     *
+     *
+     * @param location
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        currentLatitude = location.getLatitude();
+        currentLongitude = location.getLongitude();
+
+        Toast.makeText(this, currentLatitude + " WORKS " + currentLongitude + "", Toast.LENGTH_LONG).show();
+    }
+
     @Override
     public void onQuoteResponseReady(QuotePackage response) {
         Log.d("RICKY",response.getQuote());
@@ -110,7 +244,6 @@ public class WalkMode extends Activity implements Quoteable.ResponseReadyListene
     public void onQuoteResponseFailed(QuotePackage defaultResponse) {
         Log.d("RICKY",defaultResponse.getQuote());
         Log.d("RICKY",defaultResponse.getAuthor());
-        tts.speak("I am not connected to internet, Good for you, Not so much for me",TextToSpeech.QUEUE_FLUSH,null);
     }
 
 
