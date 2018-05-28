f6c77e76605a1c77dd998810cd01beffe0bbd3a9
phantom5 <rickrudra@gmail.com>
2018-05-28 13:09:05 +0530

    presentation mode dev started

---
+++ Toothless_Interact/app/src/main/java/com/example/rudra/toothless/PresentationMode.java
@@ -0,0 +1,14 @@
+package com.example.rudra.toothless;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class PresentationMode extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.presentation_mode);
+
+
+
+    }
+}
--- Toothless_Interact/app/src/main/java/com/example/rudra/toothless/SadMode.java
+++ Toothless_Interact/app/src/main/java/com/example/rudra/toothless/SadMode.java
@@ -1,7 +1,6 @@
 package com.example.rudra.toothless;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.speech.RecognizerIntent;
@@ -10,9 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 import java.util.Locale;
 import java.util.Random;
