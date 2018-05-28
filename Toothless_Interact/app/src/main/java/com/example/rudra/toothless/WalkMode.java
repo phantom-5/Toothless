ccf0d503aa96205bfbbc0b3cf7de595124274e2c
phantom5 <rickrudra@gmail.com>
2018-05-28 11:44:56 +0530

    implemented the Haversine Formula for distance cal

--- Toothless_Interact/app/src/main/java/com/example/rudra/toothless/WalkMode.java
+++ Toothless_Interact/app/src/main/java/com/example/rudra/toothless/WalkMode.java
@@ -286,21 +286,15 @@ public class WalkMode extends AppCompatActivity implements Quoteable.ResponseRea
     private double deg2rad(double deg) {
         return (deg * Math.PI / 180.0);
     }
-    private double rad2deg(double rad) {
-        return (rad * 180.0 / Math.PI);
-    }
     private double distance(double lat1, double lon1, double lat2, double lon2) {
-        double theta = lon1 - lon2;
-        double dist = Math.sin(deg2rad(lat1))
-                * Math.sin(deg2rad(lat2))
-                + Math.cos(deg2rad(lat1))
-                * Math.cos(deg2rad(lat2))
-                * Math.cos(deg2rad(theta));
-        dist = Math.acos(dist);
-        dist = rad2deg(dist);
-        dist = dist * 60 * 1.1515;
-        return (dist);
+        double R = 6371;
+        double dLat=deg2rad(lat2-lat1);
+        double dLon=deg2rad(lon2-lon1);
+        double a=Math.sin(dLat/2)*Math.sin(dLat/2)+
+                Math.cos(deg2rad(lat1))*Math.cos(deg2rad(lat2))*
+                        Math.sin(dLon/2)*Math.sin(dLon/2);
+        double c=2*Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+        double d=R*c;
+        return (d);
     }
-
-
 }
