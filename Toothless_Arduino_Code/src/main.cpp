f491369db06eeff60c1b41b475ea7a5508db21ec
phantom5 <rickrudra@gmail.com>
2018-06-01 10:39:08 +0530

    motor shield test successful

--- Toothless_Arduino_Code/src/main.cpp
+++ Toothless_Arduino_Code/src/main.cpp
@@ -1,10 +1,12 @@
 #include <Arduino.h>
 #include <AFMotor.h>
 
+AF_DCMotor mbl(1);
+
 void setup() {
-    // put your setup code here, to run once:
+    mbl.setSpeed(255);
 }
 
 void loop() {
-    
+    mbl.run(FORWARD);
 }
