1535dea20ba5772f658323eb5820c325738e4d57
phantom5 <rickrudra@gmail.com>
2018-06-02 13:32:46 +0530

    head servo alignment

--- Toothless_Arduino_Code/src/main.cpp
+++ Toothless_Arduino_Code/src/main.cpp
@@ -1,12 +1,17 @@
 #include <Arduino.h>
 #include <AFMotor.h>
+#include <Servo.h>
 
+Servo stest;
 AF_DCMotor mbl(1);
+
 
 void setup() {
     mbl.setSpeed(255);
+    stest.attach(10);
 }
 
 void loop() {
-    mbl.run(FORWARD);
+    stest.write(0);
+   // mbl.run(FORWARD);
 }
