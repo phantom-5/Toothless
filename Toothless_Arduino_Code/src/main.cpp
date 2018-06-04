4b00925a5c745ca4d27fc8bd822c675ee8577fac
phantom5 <rickrudra@gmail.com>
2018-06-04 10:47:09 +0530

    all motor directions configured

--- Toothless_Arduino_Code/src/main.cpp
+++ Toothless_Arduino_Code/src/main.cpp
@@ -3,7 +3,10 @@
 #include <Servo.h>
 
 Servo stest;
-AF_DCMotor mbl(1);
+AF_DCMotor mur(1);
+AF_DCMotor mbr(2);
+AF_DCMotor mbl(4);
+AF_DCMotor mul(3);
 int servoPos=0;
 int trigPin = A0;
 int echoPin = A1;
@@ -11,13 +14,17 @@ float cal_dis();
 
 void setup() {
     Serial.begin(9600);
+    mbr.setSpeed(255);
+    mur.setSpeed(255);
     mbl.setSpeed(255);
+    mul.setSpeed(255);
     stest.attach(10);
     pinMode(trigPin,OUTPUT);
     pinMode(echoPin,INPUT);
 }
 
 void loop() {
+    /**
     float distance;
       //0 degree is left , 180 is right , 90 is center
     for(servoPos=0;servoPos<=180;servoPos++){
@@ -41,11 +48,17 @@ void loop() {
         distance=cal_dis();
         }
         stest.write(servoPos);
+    **/
+      // mbr.run(FORWARD);
+       //mur.run(FORWARD);
+       //mbl.run(FORWARD);
+      // mul.run(FORWARD);
+
        
     }
    //stest.write(90);   //stationary state
-    
-}
+//}
+
 float cal_dis(){
 //returns distance in cm
         float duration,distance;
