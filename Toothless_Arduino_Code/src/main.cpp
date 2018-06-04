63fb0435170b2c5e5165c69aac1ae042c8ca3c70
phantom5 <rickrudra@gmail.com>
2018-06-04 19:39:01 +0530

    bug with speed fix

--- Toothless_Arduino_Code/src/main.cpp
+++ Toothless_Arduino_Code/src/main.cpp
@@ -9,7 +9,8 @@ AF_DCMotor mbl(4);
 AF_DCMotor mul(3);
 int servoPos=0;
 int trigPin = A0;
-int echoPin = A1;
+int echoPin = A5;
+int speed=0;
 float cal_dis();
 void move_forward();
 void move_backward();
@@ -26,7 +27,7 @@ void setup() {
 }
 
 void loop() {
-    
+   /**
     float distance;
       //0 degree is left , 180 is right , 90 is center
     for(servoPos=0;servoPos<=180;servoPos++){
@@ -51,12 +52,12 @@ void loop() {
         distance=cal_dis();
         }
         stest.write(servoPos);
-    
-      
-
-       
     }
+
    //stest.write(90);   //stationary state
+   **/
+     
+
 }
 
 float cal_dis(){
@@ -73,10 +74,19 @@ float cal_dis(){
 }
 
 void move_forward(){
+      Serial.println("Forward CALLED");
       mbr.run(FORWARD);
       mur.run(FORWARD);
       mbl.run(FORWARD);
       mul.run(FORWARD);
+      for(speed=0;speed<=255;speed+=2){
+        mbr.setSpeed(speed);
+        mbl.setSpeed(speed);
+        mur.setSpeed(speed);
+        mul.setSpeed(speed);
+        delay(5);
+      }
+
 }
 
 void move_backward(){
