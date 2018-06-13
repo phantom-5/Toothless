1e6d6b39412ec53cd058f9044bcc14668f0a7c69
phantom5 <rickrudra@gmail.com>
2018-06-13 15:52:03 +0530

    stable linear follow

--- Toothless_Arduino_Code/src/main.cpp
+++ Toothless_Arduino_Code/src/main.cpp
@@ -11,53 +11,47 @@ int servoPos=0;
 int trigPin = A0;
 int echoPin = A5;
 int speed=0;
+int first_control_forward=0;
 float cal_dis();
 void move_forward();
 void move_backward();
+void move_left();
+void move_right();
+void stop();
+int scan();
 
 void setup() {
     Serial.begin(9600);
-    mbr.setSpeed(255);
-    mur.setSpeed(255);
-    mbl.setSpeed(255);
-    mul.setSpeed(255);
+    mbr.setSpeed(100);
+    mur.setSpeed(100);
+    mbl.setSpeed(100);
+    mul.setSpeed(100);
     stest.attach(10);
     pinMode(trigPin,OUTPUT);
     pinMode(echoPin,INPUT);
 }
 
 void loop() {
-    /**
-   
-    float distance;
-      //0 degree is left , 180 is right , 90 is center
-    for(servoPos=0;servoPos<=180;servoPos++){
-        distance=cal_dis();
-        Serial.println(distance);
-        while(distance<=40){
-        stest.write(servoPos);
-        distance=cal_dis();
-        }
-        stest.write(servoPos);
-        //move_forward();
-      //move_backward();
-    }
     
-    for(servoPos=180;servoPos>=0;servoPos--){
-        distance=cal_dis();
-        Serial.println(distance);
-        while(distance<=40){
-        stest.write(servoPos);
-        distance=cal_dis();
-        }
-        stest.write(servoPos);
-    }
-
-   //stest.write(90);   //stationary state
-**/
-     move_forward();
-    //stest.write(0);
-
+   float distance=0;
+   stest.write(90);
+   distance=cal_dis();
+   if(distance>45){
+       while(distance>=45){
+       move_forward();
+       distance=cal_dis();
+       }
+       stop();
+   }
+   else if(distance>10 && distance<25){
+       while(distance<25){
+           move_backward();
+           distance=cal_dis();
+       }
+       stop();
+   }else{
+       stop();
+   }
 }
 
 float cal_dis(){
@@ -86,4 +80,54 @@ void move_backward(){
       mur.run(BACKWARD);
       mbl.run(BACKWARD);
       mul.run(BACKWARD);
+}
+void move_left(){
+      mbr.run(FORWARD);
+      mur.run(FORWARD);
+      mbl.run(BACKWARD);
+      mul.run(BACKWARD);
+      delay(1500);
+}
+void move_right(){
+      mbr.run(BACKWARD);
+      mur.run(BACKWARD);
+      mbl.run(FORWARD);
+      mul.run(FORWARD);
+      delay(1500);
+}
+void stop(){
+      mbr.run(RELEASE);
+      mur.run(RELEASE);
+      mbl.run(RELEASE);
+      mul.run(RELEASE);
+}
+int scan(){
+      float distance;
+      int angle;
+      //0 degree is left , 180 is right , 90 is center
+    for(servoPos=0;servoPos<=180;servoPos++){
+        distance=cal_dis();
+        Serial.println(distance);
+        while(distance<=45){
+        stest.write(servoPos);
+        angle=servoPos;
+        distance=cal_dis();
+        return angle;
+        }
+        stest.write(servoPos);
+        angle++; //move towards right
+    }
+    for(servoPos=180;servoPos>=0;servoPos--){
+        distance=cal_dis();
+        Serial.println(distance);
+        while(distance<=45){
+        stest.write(servoPos);
+        angle=servoPos;
+        distance=cal_dis();
+        return angle;
+        }
+        stest.write(servoPos);
+    
+    }
+
 }
