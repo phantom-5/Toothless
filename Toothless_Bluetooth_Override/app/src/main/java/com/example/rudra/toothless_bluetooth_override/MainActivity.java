929537263b5dc1ba7f7d185b4a3980f2484a8c8c
phantom5 <rickrudra@gmail.com>
2018-06-16 09:32:20 +0530

    stuff in bluetooth

--- Toothless_Bluetooth_Override/app/src/main/java/com/example/rudra/toothless_bluetooth_override/MainActivity.java
+++ Toothless_Bluetooth_Override/app/src/main/java/com/example/rudra/toothless_bluetooth_override/MainActivity.java
@@ -5,8 +5,11 @@ import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
-
 import java.util.Set;
 import java.util.UUID;
 
@@ -15,16 +18,43 @@ public class MainActivity extends AppCompatActivity {
     BluetoothAdapter blue=null;
     BluetoothSocket socket=null;
     Set<BluetoothDevice> paired;
+    Button cd,send,exit,override,back,right,left,up;
+    EditText send_text;
+    static int override_active=0;
     static final UUID HC_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            bluetooth_connect_device();
-        }catch(Exception e){}
-
+        cd=findViewById(R.id.cd);
+        cd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    bluetooth_connect_device();
+                }catch(Exception e){}
+            }
+        });
+        exit=findViewById(R.id.exit);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                override_active=0;
+                //if(blue.isEnabled()){
+                   // blue.disable();
+                //}
+            }
+        });
+        override=findViewById(R.id.override);
+        override.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                override_active=1;
+            }
+        });
+        //override.performClick();
+        Log.d("RICKY",Integer.toString(override_active));
     }
     private void bluetooth_connect_device() throws Exception
     {
@@ -37,8 +67,9 @@ public class MainActivity extends AppCompatActivity {
             {
                 for(BluetoothDevice bt : paired)
                 {
+
                     address=bt.getAddress().toString();name_dev = bt.getName().toString();
-                    Toast.makeText(getApplicationContext(),"Connected to Toothless", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(),"Connected to "+name_dev, Toast.LENGTH_LONG).show();
 
                 }
             }
